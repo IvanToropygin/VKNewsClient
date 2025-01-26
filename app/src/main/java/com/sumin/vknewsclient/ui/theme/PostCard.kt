60899f29574d5863a2dd4eb46f27c183ade4c898
Ivan Toropygin <itori86@mail.ru>
2025-01-26 18:41:28 +0300

    fix postCard

--- app/src/main/java/com/sumin/vknewsclient/ui/theme/PostCard.kt
+++ app/src/main/java/com/sumin/vknewsclient/ui/theme/PostCard.kt
@@ -4,12 +4,13 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -23,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,40 +41,7 @@ fun PostCard() {
         shape = RoundedCornerShape(4.dp),
         border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.onBackground)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                modifier = Modifier
-                    .size(70.dp)
-                    .padding(8.dp)
-                    .clip(CircleShape)
-                    .background(MaterialTheme.colorScheme.background),
-                painter = painterResource(R.drawable.post_comunity_thumbnail),
-                contentDescription = "Group icon"
-            )
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(
-                    text = "Уволено",
-                    fontWeight = FontWeight.Bold
-                )
-                Text(
-                    text = "14:00",
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colorScheme.onSecondary
-                )
-            }
-            Icon(
-                imageVector = Icons.Rounded.MoreVert,
-                contentDescription = "More button",
-                tint = MaterialTheme.colorScheme.onPrimary
-            )
-        }
+        PostHeader()
         Text(
             modifier = Modifier.padding(8.dp),
             text = "кабаныч, когда узнал, что если сотрудникам не платить" +
@@ -81,58 +50,94 @@ fun PostCard() {
         Image(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.background)
-                .fillMaxWidth(),
-            painter = painterResource(R.drawable.post_content_image),
-            contentDescription = "Content image"
-        )
-        Row(
-            modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 8.dp, vertical = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                .padding(horizontal = 8.dp),
+            painter = painterResource(R.drawable.post_content_image),
+            contentDescription = "Content image",
+            contentScale = ContentScale.Crop
+        )
+        PostFooter()
+    }
+}
+
+@Composable
+private fun PostHeader() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(70.dp)
+                .padding(8.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.background),
+            painter = painterResource(R.drawable.post_comunity_thumbnail),
+            contentDescription = "Group icon"
+        )
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = "Уволено",
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "14:00",
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSecondary
+            )
+        }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = "More button",
+            tint = MaterialTheme.colorScheme.onPrimary
+        )
+    }
+}
+
+@Composable
+private fun PostFooter() {
+    Row(modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically) {
+        Row(Modifier
+            .padding(8.dp)
+            .weight(1f)) {
+            IconWithText(
+                count = "206",
+                iconResId = R.drawable.ic_views_count,
+                contentDescription = "view count icon"
+            )
+        }
+        Row(
+            Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceAround
         ) {
-            Box {
-                CountAndIcon(
-                    count = "206",
-                    iconResId = R.drawable.ic_views_count,
-                    contentDescription = "view count icon"
-                )
-            }
-            Row {
-                CountAndIcon(
-                    count = "206",
-                    iconResId = R.drawable.ic_share,
-                    contentDescription = "share button icon"
-                )
-                CountAndIcon(
-                    count = "11",
-                    iconResId = R.drawable.ic_comment,
-                    contentDescription = "comment count icon"
-                )
-                CountAndIcon(
-                    count = "491",
-                    iconResId = R.drawable.ic_like,
-                    contentDescription = "like button  icon"
-                )
-            }
+            IconWithText(
+                count = "206",
+                iconResId = R.drawable.ic_share,
+                contentDescription = "share button icon"
+            )
+            IconWithText(
+                count = "11",
+                iconResId = R.drawable.ic_comment,
+                contentDescription = "comment count icon"
+            )
+            IconWithText(
+                count = "491",
+                iconResId = R.drawable.ic_like,
+                contentDescription = "like button  icon"
+            )
         }
     }
 }
 
 @Composable
-private fun CountAndIcon(count: String, iconResId: Int, contentDescription: String) {
+private fun IconWithText(iconResId: Int, count: String, contentDescription: String) {
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = count,
-            color = MaterialTheme.colorScheme.onSecondary
-        )
+        Text(text = count)
+        Spacer(Modifier.width(4.dp))
         Icon(
-            modifier = Modifier
-                .padding(8.dp)
-                .background(MaterialTheme.colorScheme.background),
             painter = painterResource(iconResId),
             contentDescription = contentDescription,
             tint = MaterialTheme.colorScheme.onSecondary
