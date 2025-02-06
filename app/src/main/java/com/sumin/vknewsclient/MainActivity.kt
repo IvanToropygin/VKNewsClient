46d85e883e7c28918b6cd7019894bcab6f66e4a3
Ivan Toropygin <itori86@mail.ru>
2025-02-06 16:29:04 +0300

    main screen

--- app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
@@ -3,16 +3,15 @@ package com.sumin.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.sumin.vknewsclient.ui.theme.PostCard
+import com.sumin.vknewsclient.ui.theme.MainScreen
 import com.sumin.vknewsclient.ui.theme.VKNewsClientTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VKNewsClientTheme(dynamicColor = false) {
-                PostCard()
-            }
+            VKNewsClientTheme { MainScreen() }
+
         }
     }
 }
