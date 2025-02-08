d0d286dd9e9f80f1b674db6fcec72017412001e9
Ivan Toropygin <itori86@mail.ru>
2025-02-08 18:51:32 +0300

    add LazyColumn

--- app/src/main/java/com/sumin/vknewsclient/MainViewModel.kt
+++ app/src/main/java/com/sumin/vknewsclient/MainViewModel.kt
@@ -6,20 +6,38 @@ import androidx.lifecycle.ViewModel
 import com.sumin.vknewsclient.ui.theme.domain.FeedPost
 import com.sumin.vknewsclient.ui.theme.domain.StatisticItem
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
-    private val _feedPost = MutableLiveData(FeedPost())
-    val feedPost : LiveData<FeedPost> get() = _feedPost
+    private val sourceList = mutableListOf<FeedPost>().apply {
+        repeat(10) { add(FeedPost(id = it)) }
+    }
 
-    fun updateCount(item: StatisticItem) {
-        val oldStats = feedPost.value?.statistics ?: throw IllegalStateException("_feedPost == null")
+    private val _feedPosts = MutableLiveData<List<FeedPost>>(sourceList)
+    val feedPosts: LiveData<List<FeedPost>> get() = _feedPosts
+
+    fun updateCount(
+        feedPost: FeedPost,
+        item: StatisticItem,
+    ) {
+        val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
+        val oldStats = feedPost.statistics
         val newStats = oldStats.toMutableList().apply {
             replaceAll { statsItem ->
-                if (statsItem.type == item.type)
-                    statsItem.copy(count = statsItem.count + 1)
+                if (statsItem.type == item.type) statsItem.copy(count = statsItem.count + 1)
                 else statsItem
             }
         }
-        _feedPost.value = feedPost.value?.copy(statistics = newStats)
+        val newFeedPost = feedPost.copy(statistics = newStats)
+        _feedPosts.value = oldPosts.apply {
+            replaceAll {
+                if (it.id == newFeedPost.id) newFeedPost else it
+            }
+        }
+    }
+
+    fun remove(feedPost: FeedPost) {
+        val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
+        oldPosts.remove(feedPost)
+        _feedPosts.value = oldPosts
     }
 }
