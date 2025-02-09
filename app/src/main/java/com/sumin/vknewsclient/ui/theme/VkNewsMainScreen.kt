d2aa94385f85a2ae8212e3af9672ed91b52c604b
Ivan Toropygin <itori86@mail.ru>
2025-02-09 12:27:37 +0300

    add navigationState

--- app/src/main/java/com/sumin/vknewsclient/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/sumin/vknewsclient/ui/theme/VkNewsMainScreen.kt
@@ -11,62 +11,55 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import com.sumin.vknewsclient.MainViewModel
 import com.sumin.vknewsclient.ui.theme.navigation.AppNavGraph
+import com.sumin.vknewsclient.ui.theme.navigation.rememberNavigationState
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
 
-    val navHostController = rememberNavController()
+    val navigationState = rememberNavigationState()
 
-    Scaffold(
-        bottomBar = {
-            NavigationBar {
-                val navBackStackEntry by navHostController.currentBackStackEntryAsState()
-                val currentRoute = navBackStackEntry?.destination?.route
-                val items = listOf(
-                    NavigationItem.Home,
-                    NavigationItem.Favourite,
-                    NavigationItem.Profile
-                )
-                items.forEach { item ->
-                    NavigationBarItem(
-                        selected = currentRoute == item.screen.route,
-                        onClick = { navHostController.navigate(item.screen.route) },
-                        icon = { Icon(item.icon, contentDescription = null) },
-                        label = { Text(text = stringResource(id = item.titleResId)) },
-                        colors = NavigationBarItemDefaults.colors(
-                            selectedIconColor = MaterialTheme.colorScheme.primary,
-                            selectedTextColor = MaterialTheme.colorScheme.primary,
-                            unselectedIconColor = MaterialTheme.colorScheme.secondary,
-                            unselectedTextColor = MaterialTheme.colorScheme.secondary
-                        )
+    Scaffold(bottomBar = {
+        NavigationBar {
+            val navBackStackEntry by navigationState.navHostController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route
+            val items = listOf(
+                NavigationItem.Home, NavigationItem.Favourite, NavigationItem.Profile
+            )
+            items.forEach { item ->
+                NavigationBarItem(
+                    selected = currentRoute == item.screen.route,
+                    onClick = { navigationState.navigateTo(item.screen.route) },
+                    icon = { Icon(item.icon, contentDescription = null) },
+                    label = { Text(text = stringResource(id = item.titleResId)) },
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = MaterialTheme.colorScheme.primary,
+                        selectedTextColor = MaterialTheme.colorScheme.primary,
+                        unselectedIconColor = MaterialTheme.colorScheme.secondary,
+                        unselectedTextColor = MaterialTheme.colorScheme.secondary
                     )
-                }
+                )
             }
         }
-    ) { paddingValues ->
-        AppNavGraph(
-            navHostController = navHostController,
+    }) { paddingValues ->
+        AppNavGraph(navHostController = navigationState.navHostController,
             homeScreenContent = { HomeScreen(viewModel, paddingValues) },
             favouriteScreenContent = { TextCounter("Favourite") },
-            profileScreenContent = { TextCounter("Profile") }
-        )
+            profileScreenContent = { TextCounter("Profile") })
     }
 }
 
 @Composable
 private fun TextCounter(name: String) {
-    var count by remember { mutableStateOf(0) }
+    var count by rememberSaveable { mutableStateOf(0) }
     Text(
-        modifier = Modifier.clickable { count++ },
-        text = "$name: $count", color = Color.Black
+        modifier = Modifier.clickable { count++ }, text = "$name: $count", color = Color.Black
     )
 }
