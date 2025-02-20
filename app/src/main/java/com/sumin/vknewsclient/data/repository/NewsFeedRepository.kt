f0a03c661a206d25ac8f059fb6de0f496e0f540d
Ivan Toropygin <itori86@mail.ru>
2025-02-20 12:38:31 +0300

    add flow in newsFeed

--- app/src/main/java/com/sumin/vknewsclient/data/repository/NewsFeedRepository.kt
+++ app/src/main/java/com/sumin/vknewsclient/data/repository/NewsFeedRepository.kt
@@ -8,37 +8,68 @@ import com.sumin.vknewsclient.domain.FeedPost
 import com.sumin.vknewsclient.domain.PostComment
 import com.sumin.vknewsclient.domain.StatisticItem
 import com.sumin.vknewsclient.domain.StatisticType
+import com.sumin.vknewsclient.extensions.mergeWith
 import com.vk.api.sdk.VKPreferencesKeyValueStorage
 import com.vk.api.sdk.auth.VKAccessToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 
 class NewsFeedRepository(application: Application) {
 
     private val storage = VKPreferencesKeyValueStorage(application)
+
     private val token = VKAccessToken.restore(storage)
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
+
+    private val nextDataNeededEvents = MutableSharedFlow<Unit>(replay = 1)
+    private val refreshedListFlow = MutableSharedFlow<List<FeedPost>>()
+    private val loadedListFlow = flow {
+        nextDataNeededEvents.emit(Unit)
+        nextDataNeededEvents.collect {
+            val startFrom = nextFrom
+            if (startFrom == null && feedPosts.isNotEmpty()) {
+                emit(feedPosts)
+                return@collect
+            }
+
+            val response = if (startFrom == null) apiService.loadRecommendations(getAccessToken())
+            else {
+                apiService.loadRecommendations(
+                    token = getAccessToken(),
+                    startFrom = startFrom
+                )
+            }
+            nextFrom = response.newsFeedContentDto.nextFrom
+            val posts = mapper.mapResponseToPost(response)
+            _feedPosts.addAll(posts)
+            emit(feedPosts)
+        }
+    }
 
     private val apiService = ApiFactory.apiService
     private val mapper = NewsFeedMapper()
 
     private val _feedPosts = mutableListOf<FeedPost>()
-    val feedPosts: List<FeedPost> get() = _feedPosts.toList()
+    private val feedPosts: List<FeedPost> get() = _feedPosts.toList()
 
     private var nextFrom: String? = null
 
-    suspend fun loadRecommendations(): List<FeedPost> {
-        val startFrom: String? = nextFrom
-        if (startFrom == null && feedPosts.isNotEmpty()) return feedPosts
+    val recommendations: StateFlow<List<FeedPost>> = loadedListFlow
+        .mergeWith(refreshedListFlow)
+        .stateIn(
+        scope = coroutineScope,
+        started = SharingStarted.Lazily,
+        initialValue = feedPosts
+    )
 
-        val response = if (startFrom == null) apiService.loadRecommendations(getAccessToken())
-        else {
-            apiService.loadRecommendations(
-                token = getAccessToken(),
-                startFrom = startFrom
-            )
-        }
-        nextFrom = response.newsFeedContentDto.nextFrom
-        val posts = mapper.mapResponseToPost(response)
-        _feedPosts.addAll(posts)
-        return feedPosts
+    suspend fun loadNextData() {
+        nextDataNeededEvents.emit(Unit)
     }
 
     suspend fun deletePost(feedPost: FeedPost) {
@@ -48,6 +79,7 @@ class NewsFeedRepository(application: Application) {
             postId = feedPost.id
         )
         _feedPosts.remove(feedPost)
+        refreshedListFlow.emit(feedPosts)
     }
 
     suspend fun getComments(feedPost: FeedPost): List<PostComment> {
@@ -91,6 +123,7 @@ class NewsFeedRepository(application: Application) {
         )
         val postIndex = _feedPosts.indexOf(feedPost)
         _feedPosts[postIndex] = newPost
+        refreshedListFlow.emit(feedPosts)
     }
 
     private fun getAccessToken() = token?.accessToken ?: throw RuntimeException("Token is null")
